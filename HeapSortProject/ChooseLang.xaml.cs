3e576e3a695282210a130bb919ccee4532923a6c
Saka02 <99744080+Saka02@users.noreply.github.com>
2022-09-29 13:30:32 +0200

    Add project files.

---
+++ HeapSortProject/ChooseLang.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HeapSortProject
+{
+    /// <summary>
+    /// Interaction logic for ChooseLang.xaml
+    /// </summary>
+    public partial class ChooseLang : Window
+    {
+        public ChooseLang()
+        {
+            InitializeComponent();
+        }
+
+        private void UKClick(object sender, RoutedEventArgs e)
+        {
+            HeapSortUK heapSortUK = new HeapSortUK();
+            heapSortUK.ShowDialog();
+        }
+
+        private void RSClick(object sender, RoutedEventArgs e)
+        {
+            HeapSortRS heapSortRS = new HeapSortRS();   
+            heapSortRS.ShowDialog();
+        }
+    }
+}
---
+++ HeapSortProject/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HeapSortProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<TextBlock> elementi = new List<TextBlock>(); 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary> ///
+        /// 
+        /// </summary>
+        /// <para
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+            TextBlock temp = new TextBlock();
+            int tempnum = 0;
+            if(int.TryParse(MainIn.Text, out tempnum))
+            {
+                temp.Text = MainIn.Text;
+                
+                temp.Background = Brushes.Black;
+                PanelS.Children.Add(temp);
+                temp.Width = 50;
+                temp.Height = 50;
+                temp.HorizontalAlignment = HorizontalAlignment.Left;
+                elementi.Add(temp);
+                temp.FontSize = 40;
+                temp.VerticalAlignment = VerticalAlignment.Center;
+                temp.TextAlignment = TextAlignment.Center;
+                temp.Margin = new Thickness(5);
+                temp.Foreground = Brushes.White;
+                
+                
+                
+            }
+            else
+            {
+                MessageBox.Show("Molim vas unesite broj");
+            }
+            ClearInput();
+        }
+
+        private void RemoveClick(object sender, RoutedEventArgs e)
+        {
+            TextBlock temp = elementi.Where(item => item.Text == MainIn.Text).FirstOrDefault();
+            PanelS.Children.Remove(temp);
+            elementi.Remove(temp);
+            ClearInput();
+        }
+
+        private void ClearClick(object sender, RoutedEventArgs e)
+        {
+            PanelS.Children.Clear();
+            elementi.Clear();
+        }
+
+        void ClearInput()
+        {
+            MainIn.Text = string.Empty;
+        }
+
+        void Heapify(int index)
+        {
+
+        }
+
+        private void SaveClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void InfoClick(object sender, RoutedEventArgs e)
+        {
+            ChooseLang chooseLang = new ChooseLang();
+            chooseLang.ShowDialog();
+
+        }
+        private void SortClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
